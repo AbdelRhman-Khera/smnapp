26281e83a7ba6d9e0026730f288ee84363fc2c9a
AbdelRhman Khera <abdokhera35@gmail.com>
2024-12-28 08:24:09 +0200

    config ngnix

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    'Samnan';
+    // return view('welcome');
+});
+Route::get('/test', function () {
+    'test';
+    // return view('welcome');
 });
