07e48e5ad8d84646f2dd23168429267fad39a398
AbdelRhman Khera <abdokhera35@gmail.com>
2026-05-16 15:55:41 +0300

    request materials

--- app/Http/Controllers/TechnicianSparePartRequestController.php
+++ app/Http/Controllers/TechnicianSparePartRequestController.php
@@ -276,7 +276,7 @@ class TechnicianSparePartRequestController extends Controller
             if ($status === 'S') {
 
                 $spareRequest->update([
-                    'status' => 'test',
+                    'status' => 'delivered',
                     'gr_response' => $responseData,
                     'delivered_at' => now(),
                 ]);
